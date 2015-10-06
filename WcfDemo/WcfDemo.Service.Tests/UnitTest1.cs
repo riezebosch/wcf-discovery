9cf4b9dcd177d337bf33a3440f39f70583947702
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-06 10:28:49 +0200

    Demo van integratie- en unit test met WCF

---
+++ WcfDemo/WcfDemo.Service.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+using WcfDemo.Contracts;
+
+namespace WcfDemo.Service.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IntegratieTestDemoVanService()
+        {
+            ServiceHost host = new ServiceHost(typeof(PingService));
+            host.AddServiceEndpoint(typeof(IService),
+                new NetNamedPipeBinding(), 
+                "net.pipe://localhost/ping");
+
+            try
+            {
+                host.Open();
+
+                var client = ChannelFactory<IService>.CreateChannel(
+                    new NetNamedPipeBinding(), 
+                    new EndpointAddress("net.pipe://localhost/ping"));
+
+                var result = client.Hello("from a unit test");
+                Assert.AreEqual("Hello from a unit test", result);
+            }
+            finally
+            {
+                host.Abort();
+            }
+        }
+
+        [TestMethod]
+        public void UntiTestDemoVanService()
+        {
+            var service = new PingService();
+
+            var result = service.Hello("from a unit test");
+            Assert.AreEqual("Hello from a unit test", result);
+        }
+    }
+}
