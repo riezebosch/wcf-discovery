b9eba7823838e4e8303d499fa4ce2b08bd3f6f3c
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-29 09:47:56 +0200

    demo van service gegenereerd vanuit Visual Studio met console application als client.

---
+++ WcfService1/WcfService1.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService1.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            var result = client.GetData(12);
+
+            Console.WriteLine(result);
+        }
+    }
+}
