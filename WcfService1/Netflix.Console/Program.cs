fd6e256e34d35d7285fe3e6a841c9e10c6d970de
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-30 10:44:11 +0200

    netnamedpipe binding in IIS met console

---
+++ WcfService1/Netflix.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Netflix.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new NetflixServiceReference.NetflixServiceClient();
+            var result = client.Top10();
+
+            WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
