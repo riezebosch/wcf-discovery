7a4ab0190ca5611e8c22d08f1751ea700d2bc0be
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-31 11:19:53 +0200

    oracle contract toegevoegd

---
+++ WcfService1/Discovery.Service/DiscoveryOracle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Discovery.Contracts;
+using System.Linq;
+
+namespace Discovery.Service
+{
+    public class DiscoveryOracle : IDiscoveryOracle
+    {
+        static Random random = new Random();
+
+        public string Ask(string question)
+        {
+            string[] answers =
+            {
+                "Je mag naar huis",
+                "Het is te duur",
+                "Tijd voor cup-a-soup",
+                "Tijd voor koffie",
+                "Ik zou voor een Apple gaan"
+            };
+
+            return answers.Skip(random.Next(answers.Length - 1)).First();
+        }
+    }
+}
