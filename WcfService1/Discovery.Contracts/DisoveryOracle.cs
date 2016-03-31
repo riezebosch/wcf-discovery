0b63de5655e68c2272ad641f2e8076ef600f0de5
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-31 09:24:52 +0200

    discovery contract

---
+++ WcfService1/Discovery.Contracts/DisoveryOracle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Contracts
+{
+    [ServiceContract(
+        Name = "Oracle",
+        Namespace = "urn:www-infosupport-com:wsdiscovery-demo")]
+    public interface DisoveryOracle
+    {
+        [OperationContract]
+        string Ask(string question);
+    }
+}
