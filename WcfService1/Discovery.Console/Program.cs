092cb55305234a5facaff11991d68ce7af943532
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-31 09:56:04 +0200

    client met dynamic endpoint, service host project voor deployment

---
+++ WcfService1/Discovery.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Discovery.Console.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Discovery.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var client = new OracleClient();
+                try
+                {
+                    var result = client.Ask("random");
+                    WriteLine(result);
+                }
+                finally
+                {
+                    if (client.State == CommunicationState.Opened)
+                    {
+                        client.Close();
+                    }
+                    else
+                    {
+                        client.Abort();
+                    }
+                }
+            }
+        }
+    }
+}
